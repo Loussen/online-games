290111e4ed44319cde73532026769baf7eaa9b7d
fhasanli <fhesenli92@gmail.com>
2018-11-21 21:02:56 +0400

    Changes

--- includes/footer.php
+++ includes/footer.php
@@ -5,13 +5,13 @@
     <div class="tsr-footer-copyright">
         <div class="tsr-container">
             <div class="tsr-row">
-                <div class="col-4 alignRight">
+                <div class="col-4 alignRight mloh">
                     Copy Ucell 2018
                 </div>
-                <div class="col-3">
+                <div class="col-3 mloh">
                     <a href="<?=SITE_PATH?>/frequently-asked-questions" style="color: #000; text-decoration: none;">FAQ</a>
                 </div>
-                <div class="col-5 align-right">
+                <div class="col-5 mloh log align-right">
                     <img src="<?=SITE_PATH.'/assets/img/GameEthio.png'?>"
                          style="/*max-height: 40px; margin-right: 20px;*/">
                 </div>
